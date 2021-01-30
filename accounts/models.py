1661f13aaa6191df648de463c3143675ee8ce7b8
AhmedElmougy <ahmed.m.elmogy@gmail.com>
2021-01-30 13:51:35 +0200

    remove User model from accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,6 @@ from django.contrib import auth
 # Create your models here.
 
 
-class User(auth.models.User,auth.models.PermissionsMixin):
 
-    def __str__(self):
-        return "@{}".format(self.username)
 
    
